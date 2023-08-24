fe76698e4f35b25b3d685fa33130e9ec362e74b6
hefeng <hefeng02@agora.io>
2023-08-24 18:35:21 +0800

    init project

---
+++ mysubmodule1/src/main/java/com/test/mysubmodule1/SubModule1.kt
@@ -0,0 +1,18 @@
+package com.test.mysubmodule1
+
+/**
+ * author : felix
+ * date : 2023/8/24
+ * description :
+ */
+class SubModule1 {
+    var name = "SubModule1"
+
+    fun setSubModuleName(name: String) {
+        this.name = name
+    }
+
+    fun getSubModuleName(): String {
+        return name
+    }
+}
